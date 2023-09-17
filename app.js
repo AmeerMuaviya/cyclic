b40d8700a954ca54b73e2e4407d653cef7fc8251
Ameer Muaviya <88961101+AmeerMuaviya@users.noreply.github.com>
2023-09-17 09:45:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
- import noteRoute from "./routers/note.js";
- import NM from "./model/note_model.js";
+import noteRoute from "./routers/note.js";
 
- const app= express();
-  dotenv.config();
-mongoose.connect(process.env.MY_URL).then((result) => {
-    
-app.get('/',  function(req,res){
-    var response={message:"'app is live now...'"}
-    res.send(response);
-});
- 
-}).catch((err) => {
-   console.log(err); 
-});
-
+const app = express();
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json())
-app.use('/note',noteRoute);
-app.listen(3000,()=>console.log('sever is running'))
+app.use(bodyParser.json());
+app.use("/note", noteRoute);
+dotenv.config();
+app.get('/',(req,res)=>{
+  res.send("hello world")
+})
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MY_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
+
+
